5675a1a5c1e81fc15ec172fe7da6cf601e797b2b
Colin Armstrong <colinwarmstrong@gmail.com>
2018-07-06 20:31:59 -0600

    Add styling for Week 2 Static Comp

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+artist = Artist.create!(name: 'Bon Jovi')
+artist.songs.create!(title: 'All The Small Things', length: 220, play_count: 10)
+artist.songs.create!(title: 'Make Damn Sure', length: 230, play_count: 11)
+artist.songs.create!(title: 'Seventy Time Seven', length: 240, play_count: 12)
+artist.songs.create!(title: 'She', length: 260, play_count: 13)
+artist.songs.create!(title: 'Helena', length: 250, play_count: 14)
+artist.songs.create!(title: 'First Date', length: 270, play_count: 15)
+artist.songs.create!(title: "Feelin' This", length: 280, play_count: 16)
+artist.songs.create!(title: 'Cute Without the E', length: 290, play_count: 17)
+artist.songs.create!(title: 'Everlong', length: 300, play_count: 18)
+artist.songs.create!(title: 'Play Crack The Sky', length: 310, play_count: 19)
