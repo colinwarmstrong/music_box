8b5280a8199327966868b62508c1a71b29eeb107
Colin Armstrong <colinwarmstrong@gmail.com>
2018-07-16 11:42:02 -0600

    Implement basic login functionality.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
   root 'welcome#index'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  # get '/logout'
 
   resources :artists, shallow: true do
     resources :songs, except: [:index]
--- spec/features/registation_spec.rb
+++ spec/features/registation_spec.rb
@@ -16,7 +16,26 @@ describe 'registration workflow' do
     click_on 'Create User'
 
     expect(current_path).to eq(user_path(User.last))
-
     expect(page).to have_content("Welcome, #{username}")
+  end
+
+  it 'submits registration form with duplicate username' do
+    username = 'pumped_to_be_here'
+    User.create(username: username, password: 'ertyiou')
+    count = User.count
+
+    visit '/'
+
+    click_on 'Sign Up to be a New User'
+
+    expect(current_path).to eq(new_user_path)
+
+    fill_in :user_username, with: username
+    fill_in :user_password, with: '12345'
+
+    click_on 'Create User'
+
+    expect(User.count).to eq(count)
+    expect(current_path).to eq(users_path)
   end
 end
